7671344f70df7efe1558b4f66ddb9fc8e54c1302
Emmanuel Camacho <ecamacho5@ucol.mx>
2023-02-09 07:58:54 -0600

    Practicas

---
+++ Practica Funciones/app.js
@@ -0,0 +1,27 @@
+function greet() {
+    console.log('Hello');
+}
+
+// functions as parameters
+function logGreeting(fn) {
+    fn();
+};
+
+// function expression
+let greetMe = function () {
+    console.log('Hello from the function expression');
+}
+
+let saludo = (nombre, tratamiento) => {
+    console.log('Hola ' + tratamiento + ' ' + nombre);
+}
+
+let cuadrado = numero => {
+    return numero * numero;
+}
+
+greet(); //Invoke
+logGreeting(greet);
+greetMe();
+saludo('Miguel', 'sr.');
+console.log(cuadrado(2));
