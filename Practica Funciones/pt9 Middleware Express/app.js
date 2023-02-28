b0f369249e13002559c44b9a1c2f6702709cd2a8
Emmanuel Camacho <ecamacho5@ucol.mx>
2023-02-28 08:11:09 -0600

    pt9 update

--- Practica Funciones/pt9 Middleware Express/app.js
+++ Practica Funciones/pt9 Middleware Express/app.js
@@ -11,5 +11,16 @@ app.get('/uno', (req, res) => {
 });
 
 app.get('/prueba', (req, res) => {
-  res.send('Hello World, from route Prueba');
+  res.send(`<!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+  </head>
+  <body>
+      ALO
+  </body>
+  </html>`);
 });
