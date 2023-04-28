e39f003702eaa5891a090b6e2dfc40e5399afbf6
EmmanuelC <ecamacho5@ucol.mx>
2023-04-28 07:50:12 -0600

    BASE ADDED

---
+++ Practica Funciones/pt20 Renderizado MongoDB/src/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const personsRoutes=require('./routes/persons');
+require('dotenv').config();
+
+mongoose.Promise = global.Promise;
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended:false}));
+app.use(personsRoutes);
+
+mongoose
+.connect(process.env.MONGODB_URI)
+.then(()=>console.log('Connected to TEST')) 
+.catch((error)=>console.error(error));
+
+app.listen(port, ()=>console.log(`Escuchando en el puerto ${port}`));
